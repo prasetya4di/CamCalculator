9313855e73bb46a990fa8b4021d837bd80d66dfd
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-16 11:23:22 +0700

    Add update database use case

---
+++ CamCalculator/Domain/UseCase/UpdateDatabaseSource.swift
@@ -0,0 +1,26 @@
+//
+//  UpdateDatabaseSource.swift
+//  CamCalculator
+//
+//  Created by Prasetya on 16/05/23.
+//
+
+import Combine
+import Foundation
+
+protocol UpdateDatabaseSource {
+    func call(_ source: DatabaseSource)
+}
+
+class UpdateDatabaseSourceImpl: UpdateDatabaseSource {
+    private let repository: SettingRepository
+    
+    init(repository: SettingRepository) {
+        self.repository = repository
+    }
+    
+    func call(_ source: DatabaseSource) {
+        let setting = Setting(databaseSource: source)
+        repository.update(source)
+    }
+}
