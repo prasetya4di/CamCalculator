ac4dabf24b2c448bf35c7bdbdf873b70f0fcae74
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-16 11:06:24 +0700

    Add scan data entity

---
+++ CamCalculator/Domain/Entity/ScanData.swift
@@ -0,0 +1,14 @@
+//
+//  ScanData.swift
+//  CamCalculator
+//
+//  Created by Prasetya on 16/05/23.
+//
+
+import Foundation
+
+struct ScanData {
+    let id: UUID
+    let result: String
+    let date: Date
+}
