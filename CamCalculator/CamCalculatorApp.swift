8a4041bfb9e478c869566fb28483813bafd371d3
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-16 10:44:18 +0700

    Initial Commit

---
+++ CamCalculator/CamCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamCalculatorApp.swift
+//  CamCalculator
+//
+//  Created by Prasetya on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CamCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
