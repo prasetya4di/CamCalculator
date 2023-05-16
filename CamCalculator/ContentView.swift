705bfd2ca89d606499a663c8c824f4d9811fb354
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-16 14:35:37 +0700

    Add camera and photo library usage description

--- CamCalculator/ContentView.swift
+++ CamCalculator/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            HomeView(
-            	scanDatas: []
-            )
+            ScannerView()
         }
     }
 }
